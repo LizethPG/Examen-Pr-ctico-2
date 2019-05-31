3c894d815aa6674cf4dfb524b053a83000927cdb
Lizeth Gonzalez <lizeth.gonzalez@Seguritech.local>
2019-05-31 15:34:19 -0500

    Bienvenido al examen final!

---
+++ finalexam/src/main/java/com/training/finalexam/finalexam/FinalexamApplication.java
@@ -0,0 +1,13 @@
+package com.training.finalexam.finalexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalexamApplication.class, args);
+	}
+
+}
